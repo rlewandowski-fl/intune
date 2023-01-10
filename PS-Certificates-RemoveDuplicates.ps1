b0a191164b3d1a9a865305cf1757cfb363b13772
rlewandowski-fl <64098830+rlewandowski-fl@users.noreply.github.com>
2023-01-10 12:15:17 -0600

    Add files via upload

--- PS-Certificates-RemoveDuplicates.ps1
+++ PS-Certificates-RemoveDuplicates.ps1
@@ -3,10 +3,10 @@ $InformationPreference = "SilentlyContinue"
 $WarningPreference = "SilentlyContinue"
 $ErrorActionPreference = "SilentlyContinue"
 
-#Find and remove duplicate certificates from personal computer certificates
+#Find and remove duplicate certificates (if exist, oldest first) from personal computer certificates
 $cert = @{}
 Get-ChildItem -Recurse Cert:\LocalMachine\My |
-    Where-Object {$_.Issuer -like "*Fole*"} |
+    Where-Object {$_.Issuer -like "*Foley*"} |
     ForEach-Object {
         $subject = $_.Subject
         If (!$cert.ContainsKey($subject)) {
@@ -18,6 +18,8 @@ Get-ChildItem -Recurse Cert:\LocalMachine\My |
 $cert.Keys | ForEach-Object {
     $duplicates = ($cert[$_] | Where-Object {$_.Count -gt 1})
     If ($duplicates) {
+		#Set exit code to success as duplicates were found and removed. This only needs to run once via InTune unless PKCS certificate configuration profiles change resulting in more duplicates.
+		$ExitCode = 0
         $duplicates.GetEnumerator() |
             Sort-Object [DateTime]"${Value.GetDateTimeString()}" -Descending |
             Select-Object -ExpandProperty Value -Skip 1 |
@@ -27,7 +29,11 @@ $cert.Keys | ForEach-Object {
                 }
             }
     }
+	Else {
+		#Set exit code to failure status as no duplicates were found and removed. This will ensure the script will run additional times via InTune to account for PKCS certificate configuration profiles that may not have applied yet.
+		$ExitCode = 1
+	}
 }
 
-#Exit successful
-Exit(0)
+#Exit 
+Exit($ExitCode)
