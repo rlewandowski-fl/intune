400ded7c1a6e3d27b944faf04c8006af4deaf59b
rlewandowski-fl <64098830+rlewandowski-fl@users.noreply.github.com>
2023-01-10 08:52:16 -0600

    Add files via upload

---
+++ PS-Certificates-RemoveDuplicates.ps1
@@ -0,0 +1,33 @@
+﻿#Suppress notifications
+$InformationPreference = "SilentlyContinue"
+$WarningPreference = "SilentlyContinue"
+$ErrorActionPreference = "SilentlyContinue"
+
+#Find and remove duplicate certificates from personal computer certificates
+$cert = @{}
+Get-ChildItem -Recurse Cert:\LocalMachine\My |
+    Where-Object {$_.Issuer -like "*Fole*"} |
+    ForEach-Object {
+        $subject = $_.Subject
+        If (!$cert.ContainsKey($subject)) {
+            $cert[$subject] = @{}
+        }
+        $cert[$subject]["$($_.Thumbprint)"] = $_
+    }
+
+$cert.Keys | ForEach-Object {
+    $duplicates = ($cert[$_] | Where-Object {$_.Count -gt 1})
+    If ($duplicates) {
+        $duplicates.GetEnumerator() |
+            Sort-Object [DateTime]"${Value.GetDateTimeString()}" -Descending |
+            Select-Object -ExpandProperty Value -Skip 1 |
+            ForEach-Object {
+                If (Test-Path $_.PSPath) {
+                    Remove-Item -Path $_.PSPath -DeleteKey
+                }
+            }
+    }
+}
+
+#Exit successful
+Exit(0)
